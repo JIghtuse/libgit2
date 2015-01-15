6fceda6869cedb6cb6e3b99028f760d8e54cc6c6
Carlos Martín Nieto <cmn@dwim.me>
2015-01-15 15:31:23 +0100

    push: remove reflog message override
    We always use "update by push".

--- tests/online/push.c
+++ tests/online/push.c
@@ -487,7 +487,7 @@ static void do_push(
 		if (check_progress_cb && expected_ret == GIT_EUSER)
 			data->transfer_progress_calls = GIT_EUSER;
 
-		error = git_remote_push(_remote, &specs, &opts, "test push");
+		error = git_remote_push(_remote, &specs, &opts);
 		git__free(specs.strings);
 
 		if (expected_ret < 0) {
